56313826c9d057ca91ffc9e13cf8b550fc47e329
최수정 <tku5mklf@ewhain.net>
2021-04-22 20:58:49 +0900

    코드 복사

--- 과제2/glob.h
+++ 과제2/glob.h
@@ -10,14 +10,14 @@
 /* HT 관련 구조체 */
 typedef struct HTentry* HTpointer;
 typedef struct HTentry {
-	int index;			// ST내 시작 인덱스
-	HTpointer next;		// 다음 id 포인터
+   int index;         // ST내 시작 인덱스
+   HTpointer next;      // 다음 id 포인터
 } HTentry;
 
 /* 에러 정의 열거형 */
 typedef enum errorTypes {
-	//문제없음, id숫자시작, id허락되지않은문자, id길이초과, 오버플로우
-	noerror, swdigit, illid, longid, overst
+   //문제없음, id숫자시작, id허락되지않은문자, id길이초과, 오버플로우
+   noerror, swdigit, illid, longid, overst
 }errorTypes;
 
 void insertID();
@@ -27,9 +27,9 @@ void printError();
 
 extern char *yytext;
 extern int yyleng;
-extern errorTypes err;			    // 현재 에러를 담고 있는 변수
-extern char ST[STsize];		        // 스트링 테이블
-extern HTpointer HT[HTsize];	    // 해쉬 테이블
-extern int start, end;		        // 현재 identifier의 ST 내 시작, 끝 위치
+extern errorTypes err;             // 현재 에러를 담고 있는 변수
+extern char ST[STsize];              // 스트링 테이블
+extern HTpointer HT[HTsize];       // 해쉬 테이블
+extern int start, end;              // 현재 identifier의 ST 내 시작, 끝 위치
 extern int lineCount;               // 라인 수
 extern int insertflag;
--- 과제2/main.c
+++ 과제2/main.c
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include "tn.h"
-extern yylex();
-extern char *yytext;
-
-void main()
-{
-    enum tokentypes tt;    /* token type */
-    printf("Line number\tToken type\tST-index\tToken\n");
-    while((tt=yylex()) != TEOF){ 
-        switch(tt){
-            case TCONST     : printf("%-22s %s\n", "TCONST", yytext); break;
-            case TELSE      : printf("%-22s %s\n", "TELSE", yytext); break;
-            case TIF        : printf("%-22s %s\n", "TIF", yytext); break;
-            case TINT       : printf("%-22s %s\n", "TINT", yytext); break;
-            case TRETURN    : printf("%-22s %s\n", "TRETURN", yytext); break;
-            case TVOID      : printf("%-22s %s\n", "TVOID", yytext); break;
-            case TWHILE     : printf("%-22s %s\n", "TWHILE", yytext); break;
-            case TEQUAL     : printf("%-22s %s\n", "TEQUAL", yytext); break;
-            case TNOTEQU    : printf("%-22s %s\n", "TNOTEQU", yytext); break;
-            case TLESSE     : printf("%-22s %s\n", "TLESSE", yytext); break;
-            case TGREATE    : printf("%-22s %s\n", "TGREATE", yytext); break;
-            case TAND       : printf("%-22s %s\n", "TAND", yytext); break;
-            case TOR        : printf("%-22s %s\n", "TOR", yytext); break;
-            case TINC       : printf("%-22s %s\n", "TINC", yytext); break;
-            case TDEC       : printf("%-22s %s\n", "TDEC", yytext); break;
-            case TADDASSIGN : printf("%-22s %s\n", "TADDASSIGN", yytext); break;
-            case TSUBASSIGN : printf("%-22s %s\n", "TSUBASSIGN", yytext); break;
-            case TMULASSIGN : printf("%-22s %s\n", "TMULASSIGN", yytext); break;
-            case TDIVASSIGN : printf("%-22s %s\n", "TDIVASSIGN", yytext); break;
-            case TMODASSIGN : printf("%-22s %s\n", "TMODASSIGN", yytext); break;
-            case TIDENT     : printf("%-22s %s\n", "TIDENT", yytext); break;
-            case TNUMBER    : printf("%-22s %s\n", "TNUMBER", yytext); break;
-        }
-    }
-}
-=======
 /*
  * main.c - 각 token에 대한 출력
  * programmer - 팀 6: 1876375정하늘, 1971039이진경, 1971051최수정
@@ -57,68 +19,68 @@ int insertflag = 0;
 * printtoken() --token type 별로 출력
 */
 void printtoken(enum tnumber tn) {
-	if (tn != TERROR) printf("%-22d", lineCount);
-	switch (tn) {
-	case TCONST: printf("%-22s%-22s%-22s", "TCONST","", yytext); break;
-	case TELSE: printf("%-22s%-22s%-22s", "TELSE", "", yytext); break;
-	case TIF: printf("%-22s%-22s%-22s", "TIF", "", yytext); break;
-	case TINT: printf("%-22s%-22s%-22s", "TINT", "", yytext); break;
-	case TRETURN: printf("%-22s%-22s%-22s", "TRETURN", "", yytext); break;
-	case TVOID: printf("%-22s%-22s%-22s", "TVOID", "", yytext); break;
-	case TWHILE: printf("%-22s%-22s%-22s", "TWHILE", "", yytext); break;
+   if (tn != TERROR) printf("%-22d", lineCount);
+   switch (tn) {
+   case TCONST: printf("%-22s%-22s%-22s", "TCONST","", yytext); break;
+   case TELSE: printf("%-22s%-22s%-22s", "TELSE", "", yytext); break;
+   case TIF: printf("%-22s%-22s%-22s", "TIF", "", yytext); break;
+   case TINT: printf("%-22s%-22s%-22s", "TINT", "", yytext); break;
+   case TRETURN: printf("%-22s%-22s%-22s", "TRETURN", "", yytext); break;
+   case TVOID: printf("%-22s%-22s%-22s", "TVOID", "", yytext); break;
+   case TWHILE: printf("%-22s%-22s%-22s", "TWHILE", "", yytext); break;
 
-	case TADD: printf("%-22s%-22s%-22s", "TADD", "", yytext); break;
-	case TSUB: printf("%-22s%-22s%-22s", "TSUB", "", yytext); break;
-	case TMUL: printf("%-22s%-22s%-22s", "TMUL", "", yytext); break;
-	case TDIV: printf("%-22s%-22s%-22s", "TDIV", "", yytext); break;
-	case TMOD: printf("%-22s%-22s%-22s", "TMOD", "", yytext); break;
+   case TADD: printf("%-22s%-22s%-22s", "TADD", "", yytext); break;
+   case TSUB: printf("%-22s%-22s%-22s", "TSUB", "", yytext); break;
+   case TMUL: printf("%-22s%-22s%-22s", "TMUL", "", yytext); break;
+   case TDIV: printf("%-22s%-22s%-22s", "TDIV", "", yytext); break;
+   case TMOD: printf("%-22s%-22s%-22s", "TMOD", "", yytext); break;
 
-	case TASSIGN: printf("%-22s%-22s%-22s", "TASSIGN", "", yytext); break;
-	case TADDASSIGN: printf("%-22s%-22s%-22s", "TADDASSIGN", "", yytext); break;
-	case TSUBASSIGN: printf("%-22s%-22s%-22s", "TSUBASSIGN", "", yytext); break;
-	case TMULASSIGN: printf("%-22s%-22s%-22s", "TMULASSIGN", "", yytext); break;
-	case TDIVASSIGN: printf("%-22s%-22s%-22s", "TDIVASSIGN", "", yytext); break;
-	case TMODASSIGN: printf("%-22s%-22s%-22s", "TMODASSIGN", "", yytext); break;
+   case TASSIGN: printf("%-22s%-22s%-22s", "TASSIGN", "", yytext); break;
+   case TADDASSIGN: printf("%-22s%-22s%-22s", "TADDASSIGN", "", yytext); break;
+   case TSUBASSIGN: printf("%-22s%-22s%-22s", "TSUBASSIGN", "", yytext); break;
+   case TMULASSIGN: printf("%-22s%-22s%-22s", "TMULASSIGN", "", yytext); break;
+   case TDIVASSIGN: printf("%-22s%-22s%-22s", "TDIVASSIGN", "", yytext); break;
+   case TMODASSIGN: printf("%-22s%-22s%-22s", "TMODASSIGN", "", yytext); break;
 
-	case TNOT: printf("%-22s%-22s%-22s", "TNOT", "", yytext); break;
-	case TAND: printf("%-22s%-22s%-22s", "TAND", "", yytext); break;
-	case TOR: printf("%-22s%-22s%-22s", "TOR", "", yytext); break;
+   case TNOT: printf("%-22s%-22s%-22s", "TNOT", "", yytext); break;
+   case TAND: printf("%-22s%-22s%-22s", "TAND", "", yytext); break;
+   case TOR: printf("%-22s%-22s%-22s", "TOR", "", yytext); break;
 
-	case TEQUAL: printf("%-22s%-22s%-22s", "TEQUAL", "", yytext); break;
-	case TNOTEQU: printf("%-22s%-22s%-22s", "TNOTEQU", "", yytext); break;
-	case TLESS: printf("%-22s%-22s%-22s", "TLESS", "", yytext); break;
-	case TGREAT: printf("%-22s%-22s%-22s", "TGREAT", "", yytext); break;
-	case TLESSE: printf("%-22s%-22s%-22s", "TLESSE", "", yytext); break;
-	case TGREATE: printf("%-22s%-22s%-22s", "TGREATE", "", yytext); break;
+   case TEQUAL: printf("%-22s%-22s%-22s", "TEQUAL", "", yytext); break;
+   case TNOTEQU: printf("%-22s%-22s%-22s", "TNOTEQU", "", yytext); break;
+   case TLESS: printf("%-22s%-22s%-22s", "TLESS", "", yytext); break;
+   case TGREAT: printf("%-22s%-22s%-22s", "TGREAT", "", yytext); break;
+   case TLESSE: printf("%-22s%-22s%-22s", "TLESSE", "", yytext); break;
+   case TGREATE: printf("%-22s%-22s%-22s", "TGREATE", "", yytext); break;
 
-	case TBRASL: printf("%-22s%-22s%-22s", "TBRASL", "", yytext); break;
-	case TBRASR: printf("%-22s%-22s%-22s", "TBRASR", "", yytext); break;
-	case TBRAML: printf("%-22s%-22s%-22s", "TBRAML", "", yytext); break;
-	case TBRAMR: printf("%-22s%-22s%-22s", "TBRAMR", "", yytext); break;
-	case TBRALL: printf("%-22s%-22s%-22s", "TBRALL", "", yytext); break;
-	case TBRALR: printf("%-22s%-22s%-22s", "TBRALR", "", yytext); break;
-	case TCOMMA: printf("%-22s%-22s%-22s", "TCOMMA", "", yytext); break;
-	case TSEMICOLON: printf("%-22s%-22s%-22s", "TSEMICOLON", "", yytext); break;
+   case TBRASL: printf("%-22s%-22s%-22s", "TBRASL", "", yytext); break;
+   case TBRASR: printf("%-22s%-22s%-22s", "TBRASR", "", yytext); break;
+   case TBRAML: printf("%-22s%-22s%-22s", "TBRAML", "", yytext); break;
+   case TBRAMR: printf("%-22s%-22s%-22s", "TBRAMR", "", yytext); break;
+   case TBRALL: printf("%-22s%-22s%-22s", "TBRALL", "", yytext); break;
+   case TBRALR: printf("%-22s%-22s%-22s", "TBRALR", "", yytext); break;
+   case TCOMMA: printf("%-22s%-22s%-22s", "TCOMMA", "", yytext); break;
+   case TSEMICOLON: printf("%-22s%-22s%-22s", "TSEMICOLON", "", yytext); break;
 
-	case TBLANK: printf("%-22s%-22s%-22s", "TBLANK", "", yytext); break;
-	case TTAB: printf("%-22s%-22s%-22s", "TTAB", "", yytext); break;
-	case TNEWLINE: printf("%-22s%-22s%-22s", "TNEWLINE", "", yytext); break;
+   case TBLANK: printf("%-22s%-22s%-22s", "TBLANK", "", yytext); break;
+   case TTAB: printf("%-22s%-22s%-22s", "TTAB", "", yytext); break;
+   case TNEWLINE: printf("%-22s%-22s%-22s", "TNEWLINE", "", yytext); break;
 
-	case TINC: printf("%-22s%-22s%-22s", "TINC", "", yytext); break;
-	case TDEC: printf("%-22s%-22s%-22s", "TDEC", "", yytext); break;
+   case TINC: printf("%-22s%-22s%-22s", "TINC", "", yytext); break;
+   case TDEC: printf("%-22s%-22s%-22s", "TDEC", "", yytext); break;
 
-	case TIDENT: { 
-		int hscode = ComputeHS(start, end);
-		int exist = LookupHS(hscode, start, end);
-		if (insertflag) insertID();
-		else deleteID();
-		printf("%-22s%-22d%-22s", "TIDENT", exist, yytext); break;
-	}
-	case TNUMBER: printf("%-22s%-22s%-22s", "TNUMBER", "", yytext); break;
-	case TRNUMBER: printf("%-22s%-22s%-22s", "TRNUMBER", "", yytext); break;
-	case TERROR: printError(); cErrors++;
-	}
-	printf("\n");
+   case TIDENT: { 
+      int hscode = ComputeHS(start, end);
+      int exist = LookupHS(hscode, start, end);
+      if (insertflag) insertID();
+      else deleteID();
+      printf("%-22s%-22d%-22s", "TIDENT", exist, yytext); break;
+   }
+   case TNUMBER: printf("%-22s%-22s%-22s", "TNUMBER", "", yytext); break;
+   case TRNUMBER: printf("%-22s%-22s%-22s", "TRNUMBER", "", yytext); break;
+   case TERROR: printError(); cErrors++;
+   }
+   printf("\n");
 }
 
 
@@ -126,13 +88,12 @@ void main() {
     enum tnumber tn;    /* token type */
     printf("%-22s%-22s%-22s%-22s\n","Line number","Token type","ST-index","Token");
     while((tn=yylex()) != TEOF){ 
-		printtoken(tn);
+      printtoken(tn);
     }
-	if (cErrors == 0) {
-		printf("No errors detected\n");
-	}
-	else {
-		printf("%d errors detected\n", cErrors);
-	}
+   if (cErrors == 0) {
+      printf("No errors detected\n");
+   }
+   else {
+      printf("%d errors detected\n", cErrors);
+   }
 }
->>>>>>> 9cb41f4ed32c8c73859f242ada5653aa19f7f673
